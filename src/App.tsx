29394bc4d94091f9ef7d33ce882b951321e9465e
Vera Furtula <verafurtula@gmail.com>
2024-01-17 21:32:11 +0100

    commit all changes

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+// import ListGroup from "./components/ListGroup";
+
+function App() {
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
+  const [alertVisible, setAlertVisility] = useState(false);
+
+  return (
+    <div>
+      {/* <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      /> */}
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisility(false)}>Vera je car!</Alert>
+      )}
+      <Button onClick={() => setAlertVisility(true)}>Veliko dugme</Button>
+    </div>
+  );
+}
+
+export default App;

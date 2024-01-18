03e03115707eeeaa3dd3528ed39618950e91e81a
Vera Furtula <verafurtula@gmail.com>
2024-01-18 15:29:27 +0100

    commit styling components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
 import { useState } from "react";
 import Alert from "./components/Alert";
 import Button from "./components/Button";
-// import ListGroup from "./components/ListGroup";
+import ListGroup from "./components/ListGroup/ListGroup";
 
 function App() {
-  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-  // const handleSelectItem = (item: string) => {
-  //   console.log(item);
-  // };
-  const [alertVisible, setAlertVisility] = useState(false);
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  //const [alertVisible, setAlertVisility] = useState(false);
 
   return (
     <div>
-      {/* <ListGroup
+      <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      /> */}
-      {alertVisible && (
+      />
+      {/* {alertVisible && (
         <Alert onClose={() => setAlertVisility(false)}>Vera je car!</Alert>
       )}
-      <Button onClick={() => setAlertVisility(true)}>Veliko dugme</Button>
+      <Button onClick={() => setAlertVisility(true)}>Veliko dugme</Button> */}
     </div>
   );
 }
